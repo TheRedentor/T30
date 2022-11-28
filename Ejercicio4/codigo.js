e115774b0bd5e734d796d298cbea8bd4b62859a6
TheRedentor <58314109+TheRedentor@users.noreply.github.com>
2022-11-28 10:53:09 +0100

    Terminado

--- Ejercicio4/codigo.js
+++ Ejercicio4/codigo.js
@@ -1,14 +1,17 @@
 var valores = [true, 5, false, "hola", "adios", 2];
 
 if (valores[3].length > valores[4].length){
-    console.log(valores[3]);
+    console.log("La palabra mas larga es: " + valores[3]);
 }
 else {
-    console.log(valores[4]);
+    console.log("La palabra mas larga es: " + valores[4]);
 }
 
-console.log(valores[1] + valores[5]);
-console.log(valores[1] - valores[5]);
-console.log(valores[1] * valores[5]);
-console.log(valores[1] / valores[5]);
-console.log(Math.pow(valores[1], valores[5]));
+console.log("Resultado true: " + valores[0]);
+console.log("Resultado false: " + valores[2]);
+
+console.log(valores[1] + " + " + valores[5] + " = " + (valores[1] + valores[5]));
+console.log(valores[1] + " - " + valores[5] + " = " + (valores[1] - valores[5]));
+console.log(valores[1] + " * " + valores[5] + " = " + valores[1] * valores[5]);
+console.log(valores[1] + " / " + valores[5] + " = " + valores[1] / valores[5]);
+console.log(valores[1] + " ^ " + valores[5] + " = " + Math.pow(valores[1], valores[5]));
